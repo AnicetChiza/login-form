c5285e4fb83429d9a8549a04c82054f4fa154f3e
AnicetChiza <chizaanicet@gmail.com>
2024-03-02 14:27:01 +0200

    add to my repository

---
+++ javascript/app.js
@@ -0,0 +1,4 @@
+let signupbtn = document.querySelector('.signupBtn');
+let signinbtn = document.querySelector('.signinBtn');
+let text = document.querySelector('text');
+let inputfield = document.querySelector('.name');
