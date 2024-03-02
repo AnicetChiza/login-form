c1dff8e0cb9b1bf67cb7ed7f9c5f16dde7b9a8dd
AnicetChiza <chizaanicet@gmail.com>
2024-03-02 15:31:06 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,19 @@
 let signupbtn = document.querySelector('.signupBtn');
 let signinbtn = document.querySelector('.signinBtn');
-let text = document.querySelector('text');
+let text = document.querySelector('.title');
 let inputfield = document.querySelector('.name');
+
+signinbtn.onclick = function() {
+    inputfield.style.display = 'none';
+    text.innerHTML = 'Sing in';
+    signupbtn.classList.add('disable');
+    signinbtn.classList.remove('disable');
+}
+
+signupbtn.onclick = function() {
+    inputfield.style.display = 'inline-block';
+    text.innerHTML = 'Create Account';
+    inputfield.style.display = '';
+    signinbtn.classList.add('disable');
+    signupbtn.classList.remove('disable');
+}
